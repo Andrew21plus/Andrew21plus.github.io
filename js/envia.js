9831c165ed8534d06cdb76fd83338afc036f43de
Andrew <andres.lagos@espoch.edu.ec>
2023-02-13 09:12:11 -0500

    Se agrega envia.js y se modifica contactenos.html

---
+++ js/envia.js
@@ -0,0 +1,8 @@
+function confirma(){
+    if(confirm('¿Deseas Enviar?')){
+        document.location='./';
+        }else{
+        alert('Operación Cancelada!')
+        document.location='./';
+         }
+}
